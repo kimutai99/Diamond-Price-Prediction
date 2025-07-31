6992ac81df6ea48885d4db4ac1b540ae164d6405
kimutai99 <korosbrian574@gmail.com>
2025-07-31 10:41:38 +0300

    added file

--- app.py
+++ app.py
@@ -40,4 +40,4 @@ def predict_datapoint():
             return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=8000)
+    app.run(debug=True, host="0.0.0.0", port=5000)
